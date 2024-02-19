1b2df8c871f3d687f8872a8a6f8333cb95356ba2
Adam Farizi <adamfarizi4@gmail.com>
2024-02-19 16:21:25 +0700

    update

---
+++ app/Http/Controllers/MasterAdminController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class MasterAdminController extends Controller
+{
+    public function index()
+    {
+        $data['title'] = 'Master Admin';
+        
+        return view('auth.master.master_admin',[
+
+        ], $data);
+    }
+}
